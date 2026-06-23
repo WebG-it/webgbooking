9025b6934a82889ccccd1f74fddfe99b92f80850
marco-galassi-bot-claude <marco.galassi.bot.github.claude@webg.it>
2026-06-23 15:59:11 +0200

    0.2.6: salva selezione calendari

--- src/com_webgbooking/admin/src/Controller/GoogleController.php
+++ src/com_webgbooking/admin/src/Controller/GoogleController.php
@@ -13,7 +13,6 @@ namespace WebG\Component\Webgbooking\Administrator\Controller;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Controller\BaseController;
-use Joomla\CMS\Session\Session;
 use Joomla\Database\DatabaseInterface;
 
 class GoogleController extends BaseController
@@ -26,6 +25,26 @@ class GoogleController extends BaseController
             $db = Factory::getContainer()->get(DatabaseInterface::class);
             $db->setQuery('DELETE FROM ' . $db->quoteName('#__webgbooking_google'))->execute();
             $this->setMessage(Text::_('COM_WEBGBOOKING_GOOGLE_DISCONNECTED'));
+        } catch (\Throwable $e) {
+            $this->setMessage($e->getMessage(), 'error');
+        }
+
+        $this->setRedirect('index.php?option=com_webgbooking&view=google');
+    }
+
+    public function savecalendars()
+    {
+        $this->checkToken();
+
+        $input   = Factory::getApplication()->getInput();
+        $clean   = fn($v) => preg_replace('/[^A-Za-z0-9@._\-]/', '', (string) $v);
+        $readRaw = (array) $input->get('read', [], 'array');
+        $read    = array_values(array_filter(array_map($clean, $readRaw)));
+        $write   = $clean($input->getString('write', 'primary'));
+
+        try {
+            $this->getModel('Google')->saveCalendars($read, $write !== '' ? $write : 'primary');
+            $this->setMessage(Text::_('COM_WEBGBOOKING_GOOGLE_CAL_SAVED'));
         } catch (\Throwable $e) {
             $this->setMessage($e->getMessage(), 'error');
         }
