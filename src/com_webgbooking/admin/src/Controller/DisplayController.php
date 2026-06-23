a224ef5ab122d711834699c6a4f22537672b77f3
marco-galassi-bot-claude <marco.galassi.bot.github.claude@webg.it>
2026-06-23 13:15:23 +0200

    component com_webgbooking 0.1.0: backend lista prenotazioni

---
+++ src/com_webgbooking/admin/src/Controller/DisplayController.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @package     com_webgbooking
+ * @copyright   (C) 2026 Marco Galassi / WebG
+ * @license     GNU General Public License version 2 or later
+ */
+
+namespace WebG\Component\Webgbooking\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+
+class DisplayController extends BaseController
+{
+    protected $default_view = 'bookings';
+}
