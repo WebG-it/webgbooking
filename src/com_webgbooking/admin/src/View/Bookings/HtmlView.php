4590c91768779211a4be6f6285ac0db85f9c6dbd
marco-galassi-bot-claude <marco.galassi.bot.github.claude@webg.it>
2026-06-23 13:15:28 +0200

    component com_webgbooking 0.1.0: backend lista prenotazioni

---
+++ src/com_webgbooking/admin/src/View/Bookings/HtmlView.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @package     com_webgbooking
+ * @copyright   (C) 2026 Marco Galassi / WebG
+ * @license     GNU General Public License version 2 or later
+ */
+
+namespace WebG\Component\Webgbooking\Administrator\View\Bookings;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+
+class HtmlView extends BaseHtmlView
+{
+    protected $items;
+    protected $pagination;
+
+    public function display($tpl = null)
+    {
+        $this->items      = $this->get('Items');
+        $this->pagination = $this->get('Pagination');
+
+        ToolbarHelper::title(Text::_('COM_WEBGBOOKING'), 'calendar');
+
+        parent::display($tpl);
+    }
+}
