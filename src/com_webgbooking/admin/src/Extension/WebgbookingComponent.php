9ceacf749a047e30ef834741d48a1ff8c7dd0ac1
marco-galassi-bot-claude <marco.galassi.bot.github.claude@webg.it>
2026-06-23 13:15:25 +0200

    component com_webgbooking 0.1.0: backend lista prenotazioni

---
+++ src/com_webgbooking/admin/src/Extension/WebgbookingComponent.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @package     com_webgbooking
+ * @copyright   (C) 2026 Marco Galassi / WebG
+ * @license     GNU General Public License version 2 or later
+ */
+
+namespace WebG\Component\Webgbooking\Administrator\Extension;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\MVCComponent;
+
+class WebgbookingComponent extends MVCComponent
+{
+}
